ca80511ddb30836ca24a2648b0f26e50fc766eb8
mochrafi <rafisetiadipura@gmail.com>
2025-07-31 13:53:38 +0700

    add: custom css based on figma

--- src/app/[locale]/page.tsx
+++ src/app/[locale]/page.tsx
@@ -2,8 +2,8 @@
 
 export default function Home() {
   return (
-    <div className="grid min-h-screen grid-rows-[20px_1fr_20px] items-center justify-items-center gap-16 p-8 pb-20 font-sans sm:p-20">
+    <h1 className="text-bazaar grid min-h-screen grid-rows-[20px_1fr_20px] items-center justify-items-center gap-16 p-8 pb-20 sm:p-20">
       Hello
-    </div>
+    </h1>
   )
 }
